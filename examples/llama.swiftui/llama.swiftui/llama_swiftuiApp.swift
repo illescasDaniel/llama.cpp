11df8520337e2327ef104e62ea92010dba09f29e
Daniel Illescas Romero <illescas.daniel@protonmail.com>
2024-01-05 12:40:23 +0100

    Observation

--- examples/llama.swiftui/llama.swiftui/llama_swiftuiApp.swift
+++ examples/llama.swiftui/llama.swiftui/llama_swiftuiApp.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 @main
 struct llama_swiftuiApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
